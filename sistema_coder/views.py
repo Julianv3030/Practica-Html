48ce60c9a8e1f72890c3df94e090e85988e88186
unknown <Neo@gmail.com>
2023-07-26 19:58:50 -0300

    practicando html con vsc

---
+++ sistema_coder/views.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def saludar(request):
+    saludo = "Hola querido usuario"
+    respuesta_http = HttpResponse(saludo)
+    return respuesta_http
+
+
+def saludar_con_fecha(request):
+    hoy = datetime.now()
+    saludo = f"Hola querido usuario, fecha: {hoy.day}/{hoy.month}"
+    respuesta_http = HttpResponse(saludo)
+    return respuesta_http
+
+def saludar_con_html(request):
+    http_response = render(
+        request=request, 
+        template_name='base.html',
+        context={},
+    )
+    return http_response
